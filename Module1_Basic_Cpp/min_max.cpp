536c864875ca0926b896b24148b6cc1cb654b75d
limon <shahariarhasanlimon@gmail.com>
2026-04-11 00:23:55 +0600

    added min and max

---
+++ Module1_Basic_Cpp/min_max.cpp
@@ -0,0 +1,9 @@
+// min() and max() Built-in function in C++
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a = 10, b = 20;
+    cout << "Minimum of " << a << " and " << b << " is: " << min(a, b) << endl;
+    cout << "Maximum of " << a << " and " << b << " is: " << max(a, b) << endl; 
+    return 0;
+}
