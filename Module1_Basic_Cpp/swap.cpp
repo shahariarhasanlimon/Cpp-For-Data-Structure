f447d77a01ecaa050dd3cd6b45f0aa3b3a7be5b4
limon <shahariarhasanlimon@gmail.com>
2026-04-11 00:25:13 +0600

    addded swap

---
+++ Module1_Basic_Cpp/swap.cpp
@@ -0,0 +1,10 @@
+// swap() Built-in Function in C++
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a = 5, b = 10;
+    cout << "Before swapping: a = " << a << ", b = " << b << endl;
+    swap(a, b);
+    cout << "After swapping: a = " << a << ", b = " << b << endl;
+    return 0;
+}
