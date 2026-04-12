49f284e35e0cf4c7ca9b1753e671073450fab05c
limon <shahariarhasanlimon@gmail.com>
2026-04-12 23:03:22 +0600

    updated object

--- Module_3_class_and_object/class_and_object.cpp
+++ Module_3_class_and_object/class_and_object.cpp
@@ -15,10 +15,18 @@ int main()
     s1.name = "John";
     s1.age = 20;
     s1.grade = 'A';
+    Student s2;
+    s2.name = "Alice";
+    s2.age = 22;
+    s2.grade = 'B';
 
     cout << "Name: " << s1.name << endl;
     cout << "Age: " << s1.age << endl;
     cout << "Grade: " << s1.grade << endl;
+
+    cout << "Name: " << s2.name << endl;
+    cout << "Age: " << s2.age << endl;
+    cout << "Grade: " << s2.grade << endl;
 
     return 0;
 }
